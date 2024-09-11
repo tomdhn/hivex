beff2cab591d88a15501d26ecf08385e41eea8c5
dhoin <tom.dhoine@student.vives.be>
2024-09-11 11:25:46 +0200

    fix pages

--- scripts/sideNav.js
+++
@@ -1,9 +0,0 @@
-function Nav() {
-    var width = document.getElementById("mySidenav").style.width;
-    if (width === "0px" || width == "") {
-      document.getElementById("mySidenav").style.width = "250px";
-    }
-    else {
-      document.getElementById("mySidenav").style.width = "0px";
-    }
-  }
